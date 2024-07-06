4e8738b1b7da493c3d1d31007622a9f9bdf50f58
researchersec <109282836+researchersec@users.noreply.github.com>
2024-07-06 17:50:25 +0200

    fixing theme

--- oldscript.js
+++ oldscript.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
-    // Fetch the JSON data
-    $.getJSON("BrotherBags.json", function(brotherBagsData) {
-        var tableData = [];
-        var names = new Set();
+    // Fetch the JSON data for character inventories and item prices
+    $.when(
+        $.getJSON("BrotherBags.json"),
+        $.getJSON("latest.json")
+    ).done(function(brotherBagsData, latestData) {
+        brotherBagsData = brotherBagsData[0];
+        latestData = latestData[0];
 
-        // Process the JSON data to fit into the table and collect unique names
+        var itemPrices = {};
+        var characterValues = {};
+
+        // Process the pricing data
+        latestData.pricing_data.forEach(function(priceData) {
+            itemPrices[priceData.itemId] = priceData.minBuyout;
+        });
+
+        // Process the JSON data and create tables for each character
         for (var name in brotherBagsData) {
             if (brotherBagsData.hasOwnProperty(name)) {
-                names.add(name);
                 for (var realm in brotherBagsData[name]) {
                     if (brotherBagsData[name].hasOwnProperty(realm)) {
                         var details = brotherBagsData[name][realm];
-                        var detailsHtml = processCharacterDetails(details);
-                        tableData.push([name, realm, detailsHtml]);
+                        var tableHtml = createCharacterTable(name, realm, details, itemPrices);
+                        $('#characters-container').append(tableHtml);
+
+                        // Calculate total value for the character
+                        var totalValue = calculateCharacterValue(details, itemPrices);
+                        characterValues[name + '-' + realm] = totalValue;
                     }
                 }
             }
@@ -20,32 +34,69 @@ $(document).ready(function() {
 
         // Populate the name filter dropdown
         var nameFilter = $('#name-filter');
-        names.forEach(function(name) {
-            nameFilter.append(new Option(name, name));
-        });
-
-        // Initialize DataTables
-        var dataTable = $('#data-table').DataTable({
-            data: tableData,
-            columns: [
-                { title: "Name" },
-                { title: "Realm" },
-                { title: "Details" }
-            ]
-        });
+        for (var character in characterValues) {
+            nameFilter.append(new Option(character, character));
+        }
 
         // Apply name filter
         $('#name-filter').on('change', function() {
             var selectedName = $(this).val();
             if (selectedName) {
-                dataTable.column(0).search('^' + selectedName + '$', true, false).draw();
+                $('.character-table-container').hide();
+                $('[data-character="' + selectedName + '"]').show();
             } else {
-                dataTable.column(0).search('').draw();
+                $('.character-table-container').show();
             }
         });
+
+        // Calculate and display statistics
+        displayStatistics(characterValues);
     });
 
-    // Function to process character details and create Wowhead links with icons
+    // Function to create a character table
+    function createCharacterTable(name, realm, details, itemPrices) {
+        var items = processCharacterDetails(details);
+
+        var html = '<div class="character-table-container" data-character="' + name + '-' + realm + '">';
+        html += '<h3>' + name + ' - ' + realm + '</h3>';
+        html += '<table class="character-table"><thead><tr><th>Item</th><th>Count</th><th>Price (g)</th><th>Total (g)</th></tr></thead><tbody>';
+
+        var totalValue = 0;
+        for (var itemId in items) {
+            if (items.hasOwnProperty(itemId)) {
+                var itemCount = items[itemId];
+                var itemDetails = findItemDetails(itemId);
+                if (itemDetails) {
+                    var itemName = itemDetails.name;
+                    var itemLink = 'https://classic.wowhead.com/item=' + itemId;
+                    var itemIcon = 'https://wow.zamimg.com/images/wow/icons/small/' + itemDetails.icon + '.jpg';
+                    var itemPrice = itemPrices[itemId] ? (itemPrices[itemId] / 10000).toFixed(2) : 'N/A';
+                    var itemTotal = itemPrices[itemId] ? (itemCount * itemPrices[itemId] / 10000).toFixed(2) : 'N/A';
+                    totalValue += itemPrices[itemId] ? itemCount * itemPrices[itemId] : 0;
+
+                    html += '<tr>';
+                    html += '<td style="text-align: left; padding-left: 10px;"><img class="icon" src="' + itemIcon + '" alt="" /> <a class="' + getItemClass(itemDetails.quality) + '" href="' + itemLink + '" rel="nofollow" target="_blank">' + itemName + '</a></td>';
+                    html += '<td>' + itemCount + '</td>';
+                    html += '<td>' + itemPrice + '</td>';
+                    html += '<td>' + itemTotal + '</td>';
+                    html += '</tr>';
+                } else {
+                    html += '<tr>';
+                    html += '<td>' + itemId + '</td>';
+                    html += '<td>' + itemCount + '</td>';
+                    html += '<td>N/A</td>';
+                    html += '<td>N/A</td>';
+                    html += '</tr>';
+                }
+            }
+        }
+
+        html += '</tbody><tfoot><tr><th colspan="3">Total</th><th>' + (totalValue / 10000).toFixed(2) + 'g</th></tr></tfoot></table>';
+        html += '</div>';
+        return html;
+    }
+
+    // Function to process character details and aggregate item quantities
     function processCharacterDetails(details) {
         var items = {};
         for (var key in details) {
@@ -55,27 +106,7 @@ $(document).ready(function() {
                 }
             }
         }
-
-        var html = '<ul>';
-        for (var itemId in items) {
-            if (items.hasOwnProperty(itemId)) {
-                var itemCount = items[itemId];
-                var itemDetails = findItemDetails(itemId);
-                if (itemDetails) {
-                    var itemIcon = itemDetails.icon;
-                    var itemName = itemDetails.name;
-                    var itemLink = 'https://classic.wowhead.com/item=' + itemId;
-                    var itemClass = getItemClass(itemDetails.quality);
-
-                    // Generate HTML for each item
-                    html += '<li>' + itemCount + 'x <a class="' + itemClass + '" href="' + itemLink + '" rel="nofollow" target="_blank"><img align="absmiddle" alt class="icon" src="https://wow.zamimg.com/images/wow/icons/small/' + itemIcon + '.jpg">' + itemName + '</a></li>';
-                } else {
-                    html += '<li>' + itemCount + 'x ' + itemId + '</li>';
-                }
-            }
-        }
-        html += '</ul>';
-        return html;
+        return items;
     }
 
     // Function to process bags and aggregate item quantities
@@ -123,5 +154,39 @@ $(document).ready(function() {
             default:
                 return 'white';
         }
+    }
+
+    // Function to calculate the total value of a character's inventory
+    function calculateCharacterValue(details, itemPrices) {
+        var items = processCharacterDetails(details);
+        var totalValue = 0;
+
+        for (var itemId in items) {
+            if (items.hasOwnProperty(itemId) && itemPrices[itemId]) {
+                totalValue += items[itemId] * itemPrices[itemId];
+            }
+        }
+
+        return totalValue;
+    }
+
+    // Function to display statistics
+    function displayStatistics(characterValues) {
+        var totalValue = 0;
+        var mostValueCharacter = '';
+        var highestValue = 0;
+
+        for (var character in characterValues) {
+            if (characterValues.hasOwnProperty(character)) {
+                totalValue += characterValues[character];
+                if (characterValues[character] > highestValue) {
+                    highestValue = characterValues[character];
+                    mostValueCharacter = character;
+                }
+            }
+        }
+
+        $('#total-value').text((totalValue / 10000).toFixed(2) + 'g');
+        $('#most-value-character').text(mostValueCharacter + ' (' + (highestValue / 10000).toFixed(2) + 'g)');
     }
 });
